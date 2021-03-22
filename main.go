5d5ad2ff5006fdb42d8fc2e2f99eaa4aa3120773
Yeon Jun Kim <yeonjunkim99@gmail.com>
2021-03-22 15:52:28 -0400

    Simplified functions

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-  "os"
+	"os"
+	"strings"
 )
 
 // Generic to each html page
 type Page struct {
-  Title string
-	Mob		string
+	Title string
+	Mob   string
 }
+
+type MobType int
+
+const (
+	Unknown = iota
+	Passive
+	Hostile
+)
 
 // Keep track of html files
 var templates = template.Must(template.ParseGlob("templates/*.html"))
@@ -20,37 +29,47 @@ var hostile_templates = template.Must(template.ParseGlob("templates/hostile/*.ht
 
 // Get port either local or heroku hosted
 func getPort() string {
-  p := os.Getenv("PORT")
-  if p != "" {
-    return ":" + p
-  }
-  return ":8080"
+	port := os.Getenv("PORT")
+	if port != "" {
+		return ":" + port
+	}
+	return ":8080"
 }
 
 func main() {
-  http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-  http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 
 	// Home
-  http.HandleFunc("/", Home)
+	http.HandleFunc("/", Home)
 
 	// Passive Mobs
-	http.HandleFunc("/Chicken/", Chicken)
-	http.HandleFunc("/Cow/", Cow)
-  http.HandleFunc("/Pig/", Pig)
-	http.HandleFunc("/Sheep/", Sheep)
-	http.HandleFunc("/Villager/", Villager)
+	passiveMobs := []string{
+		"chicken",
+		"cow",
+		"pig",
+		"sheep",
+		"villager",
+	}
+	for _, mob := range passiveMobs {
+		http.HandleFunc("/"+strings.ReplaceAll(mob, " ", "-")+"/", mobHandler(mob, Passive))
+	}
 
 	// Hostile Mobs
-	http.HandleFunc("/Blaze/",Blaze)
-  http.HandleFunc("/Creeper/",Creeper)
-	http.HandleFunc("/Ghast/",Ghast)
-	http.HandleFunc("/MagmaCube/",MagmaCube)
-	http.HandleFunc("/Skeleton/",Skeleton)
-	http.HandleFunc("/Slime/",Slime)
-	http.HandleFunc("/Zombie/",Zombie)
+	hostileMobs := []string{
+		"blaze",
+		"creeper",
+		"ghast",
+		"magma cube",
+		"skeleton",
+		"slime",
+		"zombie",
+	}
+	for _, mob := range hostileMobs {
+		http.HandleFunc("/"+strings.ReplaceAll(mob, " ", "-")+"/", mobHandler(mob, Hostile))
+	}
 
-  log.Fatal(http.ListenAndServe(getPort(), nil))
+	log.Fatal(http.ListenAndServe(getPort(), nil))
 }
 
 // Render the template to the view
@@ -58,126 +77,35 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p Page, check int) {
 	if check == 1 {
 		err := passive_templates.ExecuteTemplate(w, tmpl+".html", p)
 		if err != nil {
-	    http.Error(w, err.Error(), http.StatusInternalServerError)
-	  }
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	} else if check == 2 {
 		err := hostile_templates.ExecuteTemplate(w, tmpl+".html", p)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	} else {
-			err := templates.ExecuteTemplate(w, tmpl+".html", p)
-			if err != nil {
-		    http.Error(w, err.Error(), http.StatusInternalServerError)
-		  }
+		err := templates.ExecuteTemplate(w, tmpl+".html", p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func mobHandler(mob string, mobType MobType) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		p := Page{
+			Title: "Mobs: " + strings.Title(mob),
+			Mob:   strings.Title(mob),
+		}
+		renderTemplate(w, strings.Join(strings.Fields(strings.ToLower(mob)), ""), p, int(mobType))
 	}
 }
 
 // Render Home Template
-
 func Home(w http.ResponseWriter, r *http.Request) {
-  p := Page{
-    Title: "Minecraft Mobs",
-  }
-  renderTemplate(w, "index", p, 0)
-}
-
-// Passive Mob Templates
-
-func Chicken(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Chicken",
-    Mob: "Chicken",
-  }
-  renderTemplate(w, "chicken", p, 1)
-}
-
-func Cow(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Cow",
-    Mob: "Cow",
-  }
-  renderTemplate(w, "cow", p, 1)
-}
-
-func Pig(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Pig",
-    Mob: "Pig",
-  }
-  renderTemplate(w, "pig", p, 1)
-}
-
-func Sheep(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Sheep",
-    Mob: "Sheep",
-  }
-  renderTemplate(w, "sheep", p, 1)
-}
-
-func Villager(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Villager",
-    Mob: "Villager",
-  }
-  renderTemplate(w, "villager", p, 1)
-}
-
-// Hostile Mob Templates
-
-func Blaze(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Blaze",
-    Mob: "Blaze",
-  }
-  renderTemplate(w, "blaze", p, 2)
-}
-
-func Creeper(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Creeper",
-    Mob: "Creeper",
-  }
-  renderTemplate(w, "creeper", p, 2)
-}
-
-func Ghast(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Ghast",
-    Mob: "Ghast",
-  }
-  renderTemplate(w, "ghast", p, 2)
-}
-
-func MagmaCube(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Magma Cube",
-    Mob: "Magma Cube",
-  }
-  renderTemplate(w, "magmacube", p, 2)
-}
-
-func Skeleton(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Skeleton",
-    Mob: "Skeleton",
-  }
-  renderTemplate(w, "skeleton", p, 2)
-}
-
-func Slime(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Slime",
-    Mob: "Slime",
-  }
-  renderTemplate(w, "slime", p, 2)
-}
-
-func Zombie(w http.ResponseWriter, r *http.Request)  {
-  p := Page{
-		Title: "Mobs: Zombie",
-    Mob: "Zombie",
-  }
-  renderTemplate(w, "zombie", p, 2)
+	p := Page{
+		Title: "Minecraft Mobs",
+	}
+	renderTemplate(w, "index", p, 0)
 }
